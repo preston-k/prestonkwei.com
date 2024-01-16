53ca370f1d50a43ec41e1f7a3a80fa9f513be861
Preston <71202878+preston-k@users.noreply.github.com>
2024-01-15 17:14:21 -0800

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,21 @@
+const footer = `
+  <div class='footer-spacer'></div>
+  <div class='footer'>
+    <div id='footleft'>
+      <p>Copyright &copy; 2024 by Preston Kwei</p>
+      <p>Email Me: <a href='mailto:hello@prestonkwei.com'>hello@prestonkwei.com</a></p>
+    </div>
+    <div id='footright'>
+      <p>Let's Connect!</p>
+      <a target='_blank' href='https://www.instagram.com/preston.kwei'><img class='socialicon' src='https://asset.brandfetch.io/ido5G85nya/idru9ghYS7.svg'></a>
+      <a target='_blank' href='https://www.tiktok.com/@preston.kwei'><img class='socialicon' src='https://asset.brandfetch.io/id-0D6OFrq/id5GoNcuAF.svg'></a>
+      <a target='_blank' href='https://discord.com/users/790650078402052156'><img class='socialicon' src='https://asset.brandfetch.io/idM8Hlme1a/idyxkTGPf-.svg'></a>
+      <a target='_blank' href='https://www.linktr.ee/prestonslinks'><img class='socialicon' src='https://asset.brandfetch.io/id_tNIm05N/idf-el7ZKI.svg'></a>
+      <a target='_blank' href='https://spotify.link/I2xHQu25nDb'><img class='socialicon' src='https://asset.brandfetch.io/id20mQyGeY/idDGKt5UIT.svg'></a>
+      <a target='_blank' href='https://pin.it/3XjYFvX'><img class='socialicon' src='https://wnwreby.media.bublupcdn.com/eND93pf6pA5fpMumMdoyag/images/full_007-im-c9ba948f-861d-4659-a53f-78192e111ee8.png'></a>
+      <a target='_blank' href='https://vsco.co/prestonkwei'><img class='socialicon' src='https://wnwreby.media.bublupcdn.com/v-v4o477bPV1XfhmG0_wUg/images/full_007-im-c4104464-1ed0-415e-8bab-b1fbc5287139.png'></a>
+    </div>
+  </div>
+`
+
+document.body.innerHTML += footer;
---
+++ header.js
@@ -0,0 +1,14 @@
+const header = `
+    <div class='toc'>
+      <a href='https://prestonkwei.com'><h1 class='headbut' id='pkweihead'>PRESTON KWEI</h1></a>
+      <div class='tocbut-container' style='display:inline'>
+        <a href='https://prestonkwei.com'><button class='tocbut' style='background-color:white; color:black'>HOME</button></a>
+        <a href='links.html'><button class='tocbut'>LINKS</button></a>
+        <a href='about.html'><button class='tocbut'>ABOUT</button></a>
+        <a href='contact.html'><button class='tocbut'>CONTACT</button></a>
+      </div>
+    </div>
+    <hr class='hrmain' />
+  `
+
+// document.body.innerHTML = header + document.body.innerHTML
