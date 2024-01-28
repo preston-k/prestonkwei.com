69000034b31f9fdf1ef3acee42275e30c224240a
Preston Kwei <prestonkwei@gmail.com>
2024-01-28 15:29:05 -0800

    Updates

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 const data = await fetch("/static.json").then(x=>x.json());
 
 const version = data.version
+
 function init() {
   console.log('PrestonKwei.com')
   console.log('JS Sucessfully Loaded')
+  console.log('Version '+version)
   console.log(data)
 }
 
