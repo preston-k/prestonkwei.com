fba04ad12e9de61a3b736591dfd16eb39c879d4a
Preston Kwei <prestonkwei@gmail.com>
2024-03-15 22:11:42 -0700

    1.0.11: Updated login notification

--- script.js
+++ script.js
@@ -3,12 +3,30 @@ const data = await fetch("/static.json").then((x) => x.json());
 
 const version = data.version;
 
+function signinStat() {
+  const edit = document.getElementById('signinstat');
+  setTimeout(() => {
+    edit.style.top = '10px';
+    setTimeout(() => {
+      edit.style.top = '-100%';
+    }, 5000);
+  }, 10);
+}
 function init() {
   console.log("PrestonKwei.com");
   console.log("JS Sucessfully Loaded");
   console.log("Version " + version);
   console.log(data);
+  const urlParams = new URLSearchParams(window.location.search);
+  const status = urlParams.get('status');
+  if (status == 'loggedin') {
+    signinStat()
+    if (status !== null) {
+      const newUrl = window.location.pathname;
+      history.pushState({}, '', newUrl);
+  }
+  }
 }
-
 // LEAVE AT BOTTOM OF PAGE
 init();
+
