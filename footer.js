1d491ec1b679db26afe4cd525ec75c65679b7615
Preston Kwei <prestonkwei@gmail.com>
2024-06-12 11:21:37 -0700

    1.2.3: Mobile footer added

--- footer.js
+++ footer.js
@@ -24,6 +24,27 @@ const footer = `
       <a href='https://legal.prestonkwei.com/policies/tos' class='link-inline' target='_blank'><p class='legal-link'>Terms of Service</p></a>
     </div>
   </div>
+
+  <div id='mobile-footer'>
+    <div id='mobilefooter-socials'>
+      <a target='_blank' href='https://www.instagram.com/preston.kwei'><img class='mobilefoot-socialicon' src='https://asset.brandfetch.io/ido5G85nya/idru9ghYS7.svg'></a>
+      <a target='_blank' href='https://www.tiktok.com/@preston.kwei'><img class='mobilefoot-socialicon' src='https://asset.brandfetch.io/id-0D6OFrq/id5GoNcuAF.svg'></a>
+      <a target='_blank' href='https://discord.com/users/790650078402052156'><img class='mobilefoot-socialicon' src='https://asset.brandfetch.io/idM8Hlme1a/idyxkTGPf-.svg'></a>
+      <a target='_blank' href='https://www.linktr.ee/prestonslinks'><img class='mobilefoot-socialicon' src='https://asset.brandfetch.io/id_tNIm05N/idf-el7ZKI.svg'></a>
+      <a target='_blank' href='https://spotify.link/I2xHQu25nDb'><img class='mobilefoot-socialicon' src='https://asset.brandfetch.io/id20mQyGeY/idDGKt5UIT.svg'></a>
+      <a target='_blank' href='https://pin.it/3XjYFvX'><img class='mobilefoot-socialicon' src='https://wnwreby.media.bublupcdn.com/eND93pf6pA5fpMumMdoyag/images/full_007-im-c9ba948f-861d-4659-a53f-78192e111ee8.png'></a>
+      <a target='_blank' href='https://vsco.co/prestonkwei'><img class='mobilefoot-socialicon' src='https://wnwreby.media.bublupcdn.com/v-v4o477bPV1XfhmG0_wUg/images/full_007-im-c4104464-1ed0-415e-8bab-b1fbc5287139.png'></a>
+    </div>
+    <div id='mobilefooter-copyrightemail'>
+      <p class='mobilefoot-text'>Copyright &copy; 2024 by Preston Kwei</p>
+      <p class='mobilefoot-text'Email Me: <a href='mailto:hello@prestonkwei.com'>hello@prestonkwei.com</a></p>
+    </div>
+    <div id='mobilefoot-legal'>
+      <a href='https://legal.prestonkwei.com/policies/privacy' class='link-inline' target='_blank'><p  class='legal-link'>Privacy Policy</p></a>
+      <p class='link-inline' >⋅</p>
+      <a href='https://legal.prestonkwei.com/policies/tos' class='link-inline' target='_blank'><p class='legal-link'>Terms of Service</p></a>
+    </div>
+  </div>
 `;
 
 document.body.innerHTML += footer;
